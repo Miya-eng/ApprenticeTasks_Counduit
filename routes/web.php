701facfd21aab20b940dbcdbef3c91e75ad8a5f0
Takeru Miyabori <miyaeng@TakerunoMacBook-Pro.local>
2025-03-04 03:03:44 +0900

    step2-2

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,11 @@
 use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
-
-// Route::get('/editor', function () {
-//     return view('create_article');
-// });
+Route::get('/', [ArticleController::class, 'index'])->name('home');
 
 Route::get('/editor', [ArticleController::class, 'create'])->name('editor');
+
+Route::post('/editor', [ArticleController::class, 'store'])->name('editor.store');
 
 Route::get('/editor/article-slug-here', function () {
     return view('edit_article');
